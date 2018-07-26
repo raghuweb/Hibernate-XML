93c523d5ff12f1cc8761c9280fcf6af96f5f3fde
Raghunadh <Raghunadh@192.168.2.9>
2018-07-26 16:36:30 +0530

    26 july 4:36 PM

--- src/com/app/Test.java
+++ src/com/app/Test.java
@@ -1,11 +1,25 @@
 package com.app;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.classic.Session;
 
 public class Test {
 	public static void main(String[] args) {
-		Configuration cfg=new Configuration();
-		cfg=cfg.configure();
-		
-	}
-}
+		Configuration cfg = new Configuration();
+		cfg = cfg.configure();
+		SessionFactory sf = cfg.buildSessionFactory();
+		Session ses = sf.openSession();
+		Transaction tx = ses.beginTransaction();
+		tx.begin();
+		Student stu = new Student();
+		stu.setStuId(101);
+		stu.setStdName("Raghu");
+		stu.setStuFee(50000.565);
+
+		ses.save(stu);
+		tx.commit();
+		ses.close();
+	}// main()
+}// class
