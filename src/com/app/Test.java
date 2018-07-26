0babf2a05f6b8b364a881eb416a9b6037d85292d
Raghunadh <Raghunadh@192.168.2.9>
2018-07-26 16:20:23 +0530

    committ

--- src/com/app/Test.java
+++ src/com/app/Test.java
@@ -1,7 +1,11 @@
 package com.app;
 
+import org.hibernate.cfg.Configuration;
+
 public class Test {
 	public static void main(String[] args) {
-
+		Configuration cfg=new Configuration();
+		cfg=cfg.configure();
+		
 	}
 }
